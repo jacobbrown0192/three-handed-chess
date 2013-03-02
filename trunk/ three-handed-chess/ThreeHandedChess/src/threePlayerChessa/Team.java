eb7988ef0b5aa5752ced5ded8068acb97a68868d
jacobbrown0192@gmail.com <jacobbrown0192@gmail.com@efce6dd3-5761-5eb7-25ec-1001cba816cd>
2013-03-02 18:56:27 +0000

    Game class added. This class will run and hold almost everything vital to the game. Team completed except for add pieces to board function.

--- trunk/ three-handed-chess/ThreeHandedChess/src/threePlayerChessa/Team.java
+++ trunk/ three-handed-chess/ThreeHandedChess/src/threePlayerChessa/Team.java
@@ -8,10 +8,9 @@ public class Team {
 	private String name;
 	private Vector<MoveList> moves = new Vector<MoveList>();
 	private int number;
-	public Main unnamedMain;
-	public Menu unnamedMenu;
 	public Vector<Piece> pieces = new Vector<Piece>();
-
+	public Board theBoard;
+	
 	public int getColour() {
 		return this.colour;
 	}
@@ -27,9 +26,14 @@ public class Team {
 	public void setName(String aName) {
 		this.name = aName;
 	}
-
-	public Team() {
-		throw new UnsupportedOperationException();
+	
+	public Team(int colour, String name, int number, Vector<Piece> pieces, Board theBoard) {
+		super();
+		this.colour = colour;
+		this.name = name;
+		this.number = number;
+		this.pieces = pieces;
+		this.theBoard = theBoard;
 	}
 
 	public int getNumber() {
@@ -39,9 +43,25 @@ public class Team {
 	public void setNumber(int aNumber) {
 		this.number = aNumber;
 	}
-	public void addToMoveList(int num, int let)
-	{
+	
+	public void addToMoveList(int num, int let){
 		MoveList temp = new MoveList(num,let);
 		moves.add(temp);
 	}
+	
+	public void addToTeam(Piece addedPiece){
+		pieces.add(addedPiece);
+	}
+	
+	public void addPiecesToBoard(){
+		
+	}
+	
+	public void resetBoard(){
+		this.resetBoard();
+		this.addPiecesToBoard();		
+	}
+	
+	
+	
 }
