a29f5c07882c340ab0cb1fe41c21e8c32a759376
jacobbrown0192@gmail.com <jacobbrown0192@gmail.com@efce6dd3-5761-5eb7-25ec-1001cba816cd>
2013-03-04 19:19:23 +0000

    Board,Section,Segment are all complete and the board should be able to create a board from its constructor.

---  three-handed-chess/ThreeHandedChess/src/threePlayerChessa/Board.java
+++  three-handed-chess/ThreeHandedChess/src/threePlayerChessa/Board.java
@@ -5,47 +5,65 @@ import threePlayerChessa.Section;
 import threePlayerChessa.Piece;
 
 public class Board {
-	private Section section1;
-	private Section section2;
-	private Section section3;
-	public Main unnamed_Main_;
-	public Menu unnamed_Menu_;
 	public Vector<Section> sections = new Vector<Section>();
-	public Vector<Piece> unnamed_Piece_ = new Vector<Piece>();
-
+	public Vector<Piece> pieces = new Vector<Piece>();
+	public Vector<Team> gameTeams = new Vector<Team>();
+	
 	public Section getSection1() {
-		return this.section1;
+		return this.sections.elementAt(0);
 	}
 
 	public void setSection1(Section aSection1) {
-		this.section1 = aSection1;
+		this.sections.remove(0);
+		this.sections.add(aSection1);
 	}
 
 	public Section getSection2() {
-		return this.section2;
+		return this.sections.elementAt(1);
 	}
 
 	public void setSection2(Section aSection2) {
-		this.section2 = aSection2;
+		this.sections.remove(1);
+		this.sections.add(aSection2);
 	}
-
+	
 	public Section getSection3() {
-		return this.section3;
+		return this.sections.elementAt(2);
 	}
 
 	public void setSection3(Section aSection3) {
-		this.section3 = aSection3;
+		this.sections.remove(2);
+		this.sections.add(aSection3);
 	}
 
-	public void update() {
-		throw new UnsupportedOperationException();
+	public void addPiece(Piece aPiece){
+		pieces.add(aPiece);
 	}
+	
+/*	public void update() {
+		throw new UnsupportedOperationException();
+	}*/
 
 	public void reset() {
-		throw new UnsupportedOperationException();
+		pieces.clear();
+		for( int i = 0; i<3; i++){
+			for(int j = 0; j<2; j++){
+				for(int k =0; k<16; k++){
+					this.sections.elementAt(i).segments.elementAt(j).tiles.elementAt(k).setPiece(null);
+				}
+			}
+		}
 	}
 
-	public Board() {
-		throw new UnsupportedOperationException();
+	public Board(Vector<Team> gameTeams) {
+		this.gameTeams = gameTeams;
+		Section temp1 = new Section(4,1,4,1,8,5,gameTeams.elementAt(0),this);
+		sections.add(0, temp1);
+		Section temp2 = new Section(8,5,12,9,4,1,gameTeams.elementAt(1),this);
+		sections.add(1, temp2);
+		Section temp3 = new Section(12,9,8,5,12,9,gameTeams.elementAt(2),this);
+		sections.add(2, temp3);
 	}
+
+
 }
