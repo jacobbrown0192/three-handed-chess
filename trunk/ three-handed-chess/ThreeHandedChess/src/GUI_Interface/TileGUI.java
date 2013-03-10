8a71a927bc5216b8e5025593e93cad433456e738
christopher.howse@gmail.com <christopher.howse@gmail.com@efce6dd3-5761-5eb7-25ec-1001cba816cd>
2013-03-10 23:54:23 +0000

    - Works towards gui interaction with tiles

--- trunk/ three-handed-chess/ThreeHandedChess/src/GUI_Interface/TileGUI.java
+++ trunk/ three-handed-chess/ThreeHandedChess/src/GUI_Interface/TileGUI.java
@@ -3,17 +3,56 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class TileGUI extends JComponent
+public class TileGUI extends JPanel
 {
-	private String name;
+	public Polygon poly;
+	public int number;
+	public String section;
+	public MouseAdapter ma;
 	
-	public TileGUI(String name)
+    final static BasicStroke stroke = new BasicStroke(2.0f);
+	
+	public TileGUI(final Polygon poly, String section, int number)
 	{
-		this.name = name;
+		this.number = number;
+		this.section = section;
+		this.poly = poly;
+		this.ma = createMouseListener(number, section);
+		
+		this.addMouseListener(ma);
 	}
 	
-	public void paint(Graphics g)
+	@Override
+	protected void paintComponent(Graphics g)
 	{
+		super.paintComponent(g);
 		
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.setPaintMode();
+		g2.setPaint(Color.black);
+		g2.setStroke(stroke);
+		
+		g2.drawPolygon(poly);
+	}
+	
+	public MouseAdapter createMouseListener (final int num, final String sec)
+	{
+		MouseAdapter ma = new MouseAdapter()
+		{
+			public void mouseClicked(MouseEvent e)
+			{	
+				if(poly.contains(e.getPoint()))
+				{
+					System.out.println("Mouse Clicked");
+					System.out.print("Letter: ");
+					System.out.println(sec);
+					System.out.print("Number: ");
+					System.out.println(num);
+				}
+			}
+		};
+		
+		return ma;
 	}
 }
