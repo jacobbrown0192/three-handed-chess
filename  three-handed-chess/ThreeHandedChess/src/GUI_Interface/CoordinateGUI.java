668cf9f827366e4896dfcb724837e44078ccedf3
christopher.howse@gmail.com <christopher.howse@gmail.com@efce6dd3-5761-5eb7-25ec-1001cba816cd>
2013-03-15 18:27:27 +0000

    - BoardGUI and related classes commented and cleaned.

---  three-handed-chess/ThreeHandedChess/src/GUI_Interface/CoordinateGUI.java
+++  three-handed-chess/ThreeHandedChess/src/GUI_Interface/CoordinateGUI.java
@@ -2,21 +2,33 @@ package GUI_Interface;
 
 public class CoordinateGUI
 {
-	/**
-	 * 
+	/** Simple two integer class for handling points of the hexagonal board.
+	 * <p>Used by BoardGUI to set and access the points of the tile vertices.
 	 */
 	private static final long serialVersionUID = 1L;
 	public double x;
 	public double y;
 	
+	/** Default empty constructor.
+	 */
 	public CoordinateGUI() {}
 	
+	/** Constructor to create new CoordinateGUI.
+	 * 
+	 * @param x X value of coordinate
+	 * @param y Y value of coordinate
+	 */
     public CoordinateGUI(int x, int y)
     {
     	this.x = x;
     	this.y = y;
 	}
 
+    /** Method to expedite the initialization of an array of CoordinateGUIs
+     * 
+     * @param size The number of elements of the array.
+     * @return The initialized array of CoordinateGUIs.
+     */
 	public static CoordinateGUI[] coordinateSet(int size)
     {
         CoordinateGUI[] array = new CoordinateGUI[size];
@@ -25,6 +37,18 @@ public class CoordinateGUI
         return array;
     }
     
+	/** Method to set the x and y value of the coordinate at the center of
+	 * two other coordinates.
+	 * 
+	 * <p>Determines the index of the coordinate be set by averaging <code>indexA</code>
+	 * and <code>indexB</code>
+	 * <p>Determines the x coordinate of the new coordinate by averaging A's and B's x coordinates.
+	 * <p>Determines the y coordinate of the new coordinate by averaging A's and B's y coordinates.
+	 * 
+	 * @param array Array of coordinates being passed in.
+	 * @param indexA Index of the first coordinate being used to set the new index coordinate.
+	 * @param indexB Index of the second coordinate being used to set the new index coordinate.
+	 */
     public void setLocation (CoordinateGUI[] array, int indexA, int indexB)
     {
     	int index = (indexA + indexB)/2;
@@ -32,34 +56,42 @@ public class CoordinateGUI
     	array[index].y = (array[indexA].y + array[indexB].y)/2;
     }
     
-    public void setHexarant (CoordinateGUI[] section)
+    /** Sets the coordinates of every tile vertex in a hexarant having previously
+     * calculated the coordinates of the four corners of the hexarant.
+     * 
+     * <p>Sets the coordinates for the outer two columns of the hexarant.
+     * <p>Next, it sets the coordinates for the inner three columns of the hexarant, row by row.
+     * 
+     * @param hexarant Segment of the board whose coordinates are being calculated.
+     */
+    public void setHexarant (CoordinateGUI[] hexarant)
     {
-		section[0].setLocation(section, 0, 20);
-		section[0].setLocation(section, 0, 10);
-		section[0].setLocation(section, 10, 20);
+		hexarant[0].setLocation(hexarant, 0, 20);
+		hexarant[0].setLocation(hexarant, 0, 10);
+		hexarant[0].setLocation(hexarant, 10, 20);
 		
-		section[0].setLocation(section, 4, 24);
-		section[0].setLocation(section, 4, 14);
-		section[0].setLocation(section, 14, 24);
+		hexarant[0].setLocation(hexarant, 4, 24);
+		hexarant[0].setLocation(hexarant, 4, 14);
+		hexarant[0].setLocation(hexarant, 14, 24);
 		
-		section[0].setLocation(section, 0, 4);
-		section[0].setLocation(section, 0, 2);
-		section[0].setLocation(section, 2, 4);
+		hexarant[0].setLocation(hexarant, 0, 4);
+		hexarant[0].setLocation(hexarant, 0, 2);
+		hexarant[0].setLocation(hexarant, 2, 4);
 		
-		section[0].setLocation(section, 5, 9);
-		section[0].setLocation(section, 5, 7);
-		section[0].setLocation(section, 7, 9);
+		hexarant[0].setLocation(hexarant, 5, 9);
+		hexarant[0].setLocation(hexarant, 5, 7);
+		hexarant[0].setLocation(hexarant, 7, 9);
 		
-		section[0].setLocation(section, 10, 14);
-		section[0].setLocation(section, 10, 12);
-		section[0].setLocation(section, 12, 14);
+		hexarant[0].setLocation(hexarant, 10, 14);
+		hexarant[0].setLocation(hexarant, 10, 12);
+		hexarant[0].setLocation(hexarant, 12, 14);
 		
-		section[0].setLocation(section, 15, 19);
-		section[0].setLocation(section, 15, 17);
-		section[0].setLocation(section, 17, 19);
+		hexarant[0].setLocation(hexarant, 15, 19);
+		hexarant[0].setLocation(hexarant, 15, 17);
+		hexarant[0].setLocation(hexarant, 17, 19);
 		
-		section[0].setLocation(section, 20, 24);
-		section[0].setLocation(section, 20, 22);
-		section[0].setLocation(section, 22, 24);
+		hexarant[0].setLocation(hexarant, 20, 24);
+		hexarant[0].setLocation(hexarant, 20, 22);
+		hexarant[0].setLocation(hexarant, 22, 24);
     }
 }
---  three-handed-chess/ThreeHandedChess/src/GUI_Interface/PieceGUI.java
+++  three-handed-chess/ThreeHandedChess/src/GUI_Interface/PieceGUI.java
@@ -1,13 +1,48 @@
 package GUI_Interface;
 
-import java.awt.*;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
+import threePlayerChessa.Piece.PieceType;
+
+/** Extension of JLabel used to hold an icon for a board tile.
+ * <p>One instance for each of the 96 tiles, all owned by the instance of BoardGUI
+ * <p>ImageIcons are set  based on information retrieved by <code>setTileIcons</code>
+ * from the model's instance of the board.
+ * <p>Holds all the ImageIcon resources.
+ */
 public class PieceGUI extends JLabel
 {
 	private static final long serialVersionUID = 1L;
-	private Point tileCenter;
 	
+	private ImageIcon FTile = new ImageIcon(getClass().getResource("/resources/Free Tile.png"));
+	
+	private ImageIcon GKing = new ImageIcon(getClass().getResource("/resources/Green King.png"));
+	private ImageIcon GQueen  = new ImageIcon(getClass().getResource("/resources/Green Queen.png"));
+	private ImageIcon GBishop = new ImageIcon(getClass().getResource("/resources/Green Bishop.png"));
+	private ImageIcon GRook = new ImageIcon(getClass().getResource("/resources/Green Rook.png"));
+	private ImageIcon GKnight = new ImageIcon(getClass().getResource("/resources/Green Knight.png"));
+	private ImageIcon GPawn = new ImageIcon(getClass().getResource("/resources/Green Pawn.png"));
+	
+	private ImageIcon BKing = new ImageIcon(getClass().getResource("/resources/Blue King.png"));
+	private ImageIcon BQueen = new ImageIcon(getClass().getResource("/resources/Blue Queen.png"));
+	private ImageIcon BBishop = new ImageIcon(getClass().getResource("/resources/Blue Bishop.png"));
+	private ImageIcon BRook = new ImageIcon(getClass().getResource("/resources/Blue Rook.png"));
+	private ImageIcon BKnight = new ImageIcon(getClass().getResource("/resources/Blue Knight.png"));
+	private ImageIcon BPawn = new ImageIcon(getClass().getResource("/resources/Blue Pawn.png"));
+	
+	private ImageIcon RKing = new ImageIcon(getClass().getResource("/resources/Red King.png"));
+	private ImageIcon RQueen = new ImageIcon(getClass().getResource("/resources/Red Queen.png"));
+	private ImageIcon RBishop = new ImageIcon(getClass().getResource("/resources/Red Bishop.png"));
+	private ImageIcon RRook = new ImageIcon(getClass().getResource("/resources/Red Rook.png"));
+	private ImageIcon RKnight = new ImageIcon(getClass().getResource("/resources/Red Knight.png"));
+	private ImageIcon RPawn = new ImageIcon(getClass().getResource("/resources/Red Pawn.png"));
+	
+    /** Method to expedite the initialization of an array of PieceGUIs
+     * 
+     * @param size The number of elements of the array.
+     * @return The initialized array of PieceGUIs.
+     */
 	public static PieceGUI[] pieceSet(int size)
 	{
 		PieceGUI[] array = new PieceGUI[size];
@@ -18,15 +53,99 @@ public class PieceGUI extends JLabel
 		return array;
 	}
 	
-	public void setTileCenter(Point tileCenter)
+	/** Method to change the icon of a piece label.
+	 * 
+	 * <p>Called in loop by <code>setTileIcons()</code>
+	 * 
+	 * @param type Type of piece being set to the PieceGUI
+	 * @param teamNumber Owner of the piece.
+	 */
+	public void setPieceImage(PieceType type, int teamNumber)
 	{
-		this.tileCenter = tileCenter;
-	}
-	
-	@Override
-	public void paintComponent(Graphics g)
-	{
-		super.paintComponent(g);
+		switch (teamNumber)
+		{
+			case 1:
+				switch(type)
+				{
+					case PAWN:
+						this.setIcon(RPawn);
+						break;
+					case ROOK:
+						this.setIcon(RRook);
+						break;
+					case KNIGHT:
+						this.setIcon(RKnight);
+						break;
+					case BISHOP:
+						this.setIcon(RBishop);
+						break;
+					case QUEEN:
+						this.setIcon(RQueen);
+						break;
+					case KING:
+						this.setIcon(RKing);
+						break;
+					case NONE:
+						this.setIcon(FTile);
+						break;
+				}
+				break;
+			case 2:
+				switch(type)
+				{
+					case PAWN:
+						this.setIcon(BPawn);
+						break;
+					case ROOK:
+						this.setIcon(BRook);
+						break;
+					case KNIGHT:
+						this.setIcon(BKnight);
+						break;
+					case BISHOP:
+						this.setIcon(BBishop);
+						break;
+					case QUEEN:
+						this.setIcon(BQueen);
+						break;
+					case KING:
+						this.setIcon(BKing);
+						break;
+					case NONE:
+						this.setIcon(FTile);
+						break;
+				}
+				break;
+			case 3:
+				switch(type)
+				{
+					case PAWN:
+						this.setIcon(GPawn);
+						break;
+					case ROOK:
+						this.setIcon(GRook);
+						break;
+					case KNIGHT:
+						this.setIcon(GKnight);
+						break;
+					case BISHOP:
+						this.setIcon(GBishop);
+						break;
+					case QUEEN:
+						this.setIcon(GQueen);
+						break;
+					case KING:
+						this.setIcon(GKing);
+						break;
+					case NONE:
+						this.setIcon(FTile);
+						break;
+				}
+				break;
+			case 4:
+				this.setIcon(FTile);
+				break;
+		}
 	}
 	
 }
