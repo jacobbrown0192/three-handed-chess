d7733e771e0de864daf2223fb4cbdab7b83beeab
christopher.howse@gmail.com <christopher.howse@gmail.com@efce6dd3-5761-5eb7-25ec-1001cba816cd>
2013-03-20 18:42:43 +0000

    - King possibleMoves() is done.

---  three-handed-chess/ThreeHandedChess/src/threePlayerChessa/King.java
+++  three-handed-chess/ThreeHandedChess/src/threePlayerChessa/King.java
@@ -113,5 +113,111 @@ public class King extends Piece {
 			return;
 		}
 	}
+	
+	public void possibleMoves()
+	{
+		this.validTileMoves.clear();
+		Tile temp = this.currentTile;
+		
+		//Up Check
+		//Check if there's nobody there or if the piece there belongs to the enemy
+		if(temp.up.getPiece() == null || (temp.up.getPiece() != null && temp.up.getPiece().getPlayer() != this.getPlayer()))
+		{
+			this.validTileMoves.add(temp.up);
+		}
+		
+		//Down Check
+		//Check if there's nobody there or if the piece there belongs to the enemy
+		if(temp.down.getPiece() == null || (temp.down.getPiece() != null && temp.down.getPiece().getPlayer() != this.getPlayer()))
+		{
+			this.validTileMoves.add(temp.down);
+		}
+		
+		//Left Check
+		//Check if there's nobody there or if the piece there belongs to the enemy
+		if(temp.left.getPiece() == null || (temp.left.getPiece() != null && temp.left.getPiece().getPlayer() != this.getPlayer()))
+		{
+			this.validTileMoves.add(temp.left);
+		}
+		
+		//Right Check
+		//Check if there's nobody there or if the piece there belongs to the enemy
+		if(temp.right.getPiece() == null || (temp.right.getPiece() != null && temp.right.getPiece().getPlayer() != this.getPlayer()))
+		{
+			this.validTileMoves.add(temp.right);
+		}
+		
+		//Downleft Check
+		//Check if there's nobody there or if the piece there belongs to the enemy
+		if(temp.downleft.getPiece() == null || (temp.downleft.getPiece() != null && temp.downleft.getPiece().getPlayer() != this.getPlayer()))
+		{
+			this.validTileMoves.add(temp.downleft);
+		}
+		
+		//Downright Check
+		//Check if there's nobody there or if the piece there belongs to the enemy
+		if(temp.downright.getPiece() == null || (temp.downright.getPiece() != null && temp.downright.getPiece().getPlayer() != this.getPlayer()))
+		{
+			this.validTileMoves.add(temp.downright);
+		}
+		
+		//Upleft Check
+		//Check if there's nobody there or if the piece there belongs to the enemy
+		if(temp.upleft.getPiece() == null || (temp.upleft.getPiece() != null && temp.upleft.getPiece().getPlayer() != this.getPlayer()))
+		{
+			this.validTileMoves.add(temp.upleft);
+		}
+		
+		//Upright Check
+		//Check if there's nobody there or if the piece there belongs to the enemy
+		if(temp.upright.getPiece() == null || (temp.upright.getPiece() != null && temp.upright.getPiece().getPlayer() != this.getPlayer()))
+		{
+			this.validTileMoves.add(temp.upright);
+		}
+		
+		//Hop Check
+		//Check if there's nobody there or if the piece there belongs to the enemy
+		if(temp.hop.getPiece() == null || (temp.hop.getPiece() != null && temp.hop.getPiece().getPlayer() != this.getPlayer()))
+		{
+			this.validTileMoves.add(temp.hop);
+		}
+		
+		//Special Up Check
+		//Check if there's nobody there or if the piece there belongs to the enemy
+		if(temp.specialup.getPiece() == null || (temp.specialup.getPiece() != null && temp.specialup.getPiece().getPlayer() != this.getPlayer()))
+		{
+			this.validTileMoves.add(temp.specialup);
+		}
+		
+		//Special Down Check
+		//Check if there's nobody there or if the piece there belongs to the enemy
+		if(temp.specialdown.getPiece() == null || (temp.specialdown.getPiece() != null && temp.specialdown.getPiece().getPlayer() != this.getPlayer()))
+		{
+			this.validTileMoves.add(temp.specialdown);
+		}
+		
+		//Special Left Check
+		//Check if there's nobody there or if the piece there belongs to the enemy
+		if(temp.specialleft.getPiece() == null || (temp.specialleft.getPiece() != null && temp.specialleft.getPiece().getPlayer() != this.getPlayer()))
+		{
+			this.validTileMoves.add(temp.specialleft);
+		}
+		
+		//Special Right Check
+		//Check if there's nobody there or if the piece there belongs to the enemy
+		if(temp.specialright.getPiece() == null || (temp.specialright.getPiece() != null && temp.specialright.getPiece().getPlayer() != this.getPlayer()))
+		{
+			this.validTileMoves.add(temp.specialright);
+		}
+	}
 
 }
+
+
+
+
+
+
+
+
+
