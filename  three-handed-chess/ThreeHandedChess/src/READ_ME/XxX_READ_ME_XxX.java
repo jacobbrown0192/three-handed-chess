a587a7a64bfa675c857ba7682dc16f2df0074bab
jacobbrown0192@gmail.com <jacobbrown0192@gmail.com@efce6dd3-5761-5eb7-25ec-1001cba816cd>
2013-08-26 03:54:30 +0000

    added a read me so i know problems issues and possible what to do next.

---
+++  three-handed-chess/ThreeHandedChess/src/READ_ME/XxX_READ_ME_XxX.java
@@ -0,0 +1,28 @@
+
+/*
+ * !!!!!!!!!!!!!!!!CURRENT ISSUES!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * 
+ * Crashs when promoted pieces win the game(possibly only with computers)
+ * crashs randomly with some pieces because of move set or piece selection or something(computer players piece or move selection)
+ * 
+ * 
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
