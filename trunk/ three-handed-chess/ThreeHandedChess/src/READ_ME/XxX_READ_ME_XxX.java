028a92ba7c346cb557a97787903dca271dc2fd54
jacobbrown0192@gmail.com <jacobbrown0192@gmail.com@efce6dd3-5761-5eb7-25ec-1001cba816cd>
2013-08-26 03:58:23 +0000

    more read me

--- trunk/ three-handed-chess/ThreeHandedChess/src/READ_ME/XxX_READ_ME_XxX.java
+++ trunk/ three-handed-chess/ThreeHandedChess/src/READ_ME/XxX_READ_ME_XxX.java
@@ -3,7 +3,9 @@
  * !!!!!!!!!!!!!!!!CURRENT ISSUES!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
  * 
  * Crashs when promoted pieces win the game(possibly only with computers)
- * crashs randomly with some pieces because of move set or piece selection or something(computer players piece or move selection)
+ * crashs randomly with some pieces because of move set or piece selection or something
+ 	(computer players piece or move selection) check pawn promotion and how promoted pieces are handled
+ 	check how moves are located
  * 
  * 
  * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
